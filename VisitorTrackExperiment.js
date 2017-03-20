46e7c59f34cf70140209300e6285a8b7aa682032
tomas <Tom@dfr.local>
2017-03-20 13:37:30 +0100

    add clear google analytics cookies function

--- VisitorTrackExperiment.js
+++ VisitorTrackExperiment.js
@@ -62,8 +62,8 @@ loadScripts([
 
     // get visitor name from element
     jQuery(window).bind('load', function() { 
-         var nm = $( "#experiment-name" ).text();
-       var st = $( "#experiment-set" ).text();
+         var nm = jQuery( "#experiment-name" ).text();
+       var st = jQuery( "#experiment-set" ).text();
        visitor_nickname = nm + "-" + st; 
       
     });
@@ -164,12 +164,67 @@ function deleteAllVisitorIdentifier() {
     localStorage.removeItem('mid');
     sessionStorage.removeItem('sid');
 
+    var cookies = document.cookie.split(";");
+    for(var i=0; i < cookies.length; i++) {
+        var equals = cookies[i].indexOf("=");
+        var name = equals > -1 ? cookies[i].substr(0, equals) : cookies[i];
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+
+    clearCookie('__utma','.penazenkaren.sk','/');
+    clearCookie('__utmz','.penazenkaren.sk','/');
+
     reloadPage()
 }
 
 function reloadPage() {
     location.reload(true);
 }
+
+function clearCookie(name, domain, path){
+    try {
+        function Get_Cookie( check_name ) {
+                // first we'll split this cookie up into name/value pairs
+                // note: document.cookie only returns name=value, not the other components
+                var a_all_cookies = document.cookie.split(';'),
+                    a_temp_cookie = '',
+                    cookie_name = '',
+                    cookie_value = '',
+                    b_cookie_found = false;
+        
+                for ( i = 0; i < a_all_cookies.length; i++ ) {
+                    // now we'll split apart each name=value pair
+                    a_temp_cookie = a_all_cookies[i].split( '=' );
+    
+                    // and trim left/right whitespace while we're at it
+                    cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
+    
+                    // if the extracted name matches passed check_name
+                    if ( cookie_name == check_name ) {
+                        b_cookie_found = true;
+                        // we need to handle case where cookie has no value but exists (no = sign, that is):
+                        if ( a_temp_cookie.length > 1 ) {
+                            cookie_value = unescape( a_temp_cookie[1].replace(/^\s+|\s+$/g, '') );
+                        }
+                        // note that in cases where cookie is initialized but no value, null is returned
+                        return cookie_value;
+                        break;
+                    }
+                    a_temp_cookie = null;
+                    cookie_name = '';
+                }
+                if ( !b_cookie_found ) {
+                  return null;
+                }
+            }
+            if (Get_Cookie(name)) {
+                var domain = domain || document.domain;
+                var path = path || "/";
+                document.cookie = name + "=; expires=" + new Date + "; domain=" + domain + "; path=" + path;
+            }
+    }
+    catch(err) {}    
+};
 
 // sorting function by http://www.java2s.com/Tutorial/JavaScript/0220__Array/Usinganalphabeticalsortmethodonstrings.htm
 function alphabetical(a, b) {
